f22dd22360265d5eec09532a11e66d345645b090
humberto delgado <dehumberto@gmail.com>
2020-05-21 18:35:49 -0500

    Add validations

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,3 +1,7 @@
 class Trainer < ApplicationRecord
     has_and_belongs_to_many :pokemons
+    validates :name, :gender, presence: true
+    validates :team_member_status, inclusion: {in: [true, false]} 
+    validates :name, uniqueness: true
+    validates :wins, :losses, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
