87b9243a56b9e26610f9d587f09048fb559d6fce
Albert <acastemoreno@gmail.com>
2020-05-21 16:32:20 -0500

    Seed trainers and pokemons

--- db/migrate/20200521174232_create_trainer_pokemon.rb
+++ db/migrate/20200521174232_create_trainer_pokemon.rb
@@ -1,6 +1,6 @@
 class CreateTrainerPokemon < ActiveRecord::Migration[6.0]
   def change
-    create_table :trainers_pokemons do |t|
+    create_table :pokemons_trainers do |t|
       t.references :pokemon, null: false, foreign_key: true
       t.references :trainer, null: false, foreign_key: true
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-pokemon = Pokemon.create(name: "Pikachu")
-p pokemon.trainers
-trainer = Trainer.create(name: "Ash")
-trainer.pokemons << pokemon
-p trainer.pokemons
+require 'csv'
+
+File.open("db/pokemon-lite-api.csv", "r") do |file|
+  lines =  CSV.parse(file.read, headers: true, header_converters: :symbol)
+
+  trainers = lines
+  .map do |line| 
+    {
+      name: line[:t_name],
+      gender:line[:t_gender],
+      home_region: line[:t_region],
+      team_member_status: line[:t_team_member] == "true" ? "active" : "inactive",
+      wins: line[:wins],
+      losses: line[:losses]
+    }
+  end
+  .uniq
+  .map {|trainer| Trainer.find_or_create_by(trainer)}
+
+  pokemons = lines
+  .map do |line| 
+    {
+      name: line[:name],
+      base_experience: line[:base_exp].to_i,
+      main_type: line[:main_type],
+      main_ability: line[:main_ability]
+    }
+  end
+  .uniq
+  .map {|pokemon| Pokemon.find_or_create_by(pokemon)}
+
+  trainers.each do |trainer|
+    pokemon_for_trainer = lines
+    .select {|line| line[:t_name] == trainer.name }
+    .map{ |pokemon_line| pokemons.find{|pokemon| pokemon.name == pokemon_line[:name] }}
+
+    trainer.pokemons = pokemon_for_trainer
+  end
+end
+
+# pokemon = Pokemon.create(name: "Pikachu")
+# p pokemon.trainers
+# trainer = Trainer.create(name: "Ash")
+# trainer.pokemons << pokemon
+# p trainer.pokemons
