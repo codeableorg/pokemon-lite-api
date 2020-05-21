015fc149497c5e6fc3fe951540e83cacb5d19fe4
Jorge Gonzalez <jorg3.594@gmail.com>
2020-05-21 16:15:56 -0500

    validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,6 @@
 class Pokemon < ApplicationRecord
+  validates :main_type, inclusion: %w[bug electric fairy fighting fire flying grass ground ice normal poison psychic rock steel water]
+  validates :name, presence: true
+  validates :experience, presence: true
+  validates :main_ability, presence: true
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,2 +1,9 @@
 class Trainer < ApplicationRecord
+  attr_default :status, false
+  attr_default :losses, 0
+  attr_default :wins, 0
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :gender, presence: true
+  validates :region, presence: true
 end
