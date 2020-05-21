1b48cd4a060891928558a8c3cf39ed98923f3a8b
Freddy Munive <munivef@yahoo.com>
2020-05-21 12:51:40 -0500

    adding models

---
+++ db/migrate/20200521174232_create_trainer_pokemon.rb
@@ -0,0 +1,8 @@
+class CreateTrainerPokemon < ActiveRecord::Migration[6.0]
+  def change
+    create_table :trainer_pokemons do |t|
+      t.references :pokemon, null: false, foreign_key: true
+      t.references :trainer, null: false, foreign_key: true
+    end
+  end
+end
