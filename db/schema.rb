7d100c206ea5799a2a7c126913502af21ff56c74
BenMaRe-Dev <b.maguinareyes@gmail.com>
2020-05-21 16:05:01 -0500

    Create and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_173336) do
+ActiveRecord::Schema.define(version: 2020_05_21_204931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,8 @@ ActiveRecord::Schema.define(version: 2020_05_21_173336) do
   create_table "trainers", force: :cascade do |t|
     t.string "name"
     t.string "gender"
-    t.integer "age"
     t.string "region"
-    t.boolean "status"
+    t.boolean "team_member"
     t.integer "wins"
     t.integer "losses"
     t.datetime "created_at", precision: 6, null: false
