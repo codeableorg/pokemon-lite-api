e72c90891ab82113c3205d1f3676ffdb4d9d1050
Freddy Munive <munivef@yahoo.com>
2020-05-21 17:12:10 -0500

    adding validate true|false to trainer

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,7 @@
 class Trainer < ApplicationRecord
     has_and_belongs_to_many :pokemons 
     validates :name, presence: true, uniqueness: true
-    validates :genre, presence: true
+    validates :gender, presence: true
     validates :wins, :losses, numericality: {greater_than_or_equal_to: 0}
+    validates :team_member_status, inclusion: {in: [true, false]}
 end
