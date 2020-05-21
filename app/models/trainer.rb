82c24170947ca9247f7cd263beb8f4195f10ca5e
Jorge Gonzalez <jorg3.594@gmail.com>
2020-05-21 16:30:41 -0500

    validates

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -4,7 +4,6 @@ class Trainer < ApplicationRecord
   attr_default :losses, 0
   attr_default :wins, 0
   validates :name, presence: true
-  validates :age, presence: true
   validates :gender, presence: true
   validates :region, presence: true
 end
