d51628a4f457094807f5ac70d80105fc00f4e62a
Jorge Gonzalez <jorg3.594@gmail.com>
2020-05-21 16:29:16 -0500

    validaciones

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,6 @@
 class Trainer < ApplicationRecord
   has_and_belongs_to_many :pokemons
-  attr_default :status, false
+  attr_default :team_member, false
   attr_default :losses, 0
   attr_default :wins, 0
   validates :name, presence: true
