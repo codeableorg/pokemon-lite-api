ec272a3cdde3fc1cbee106e7df6342040195966c
Erick Saavedra <esvdrah@gmail.com>
2020-05-21 22:52:21 -0500

    review seeds.rg

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,24 +9,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 puts 'seeds.rb begins here'
-data = CSV.parse('db/pokemon-lite-api.csv', {headers: true, header_converters: :symbol})
+data = CSV.read('db/pokemon-lite-api.csv', { headers: true, header_converters: :symbol })
 data.each do |row|
-    pokemon = Pokemon.find_or_create_by(
-        name:row[:name], 
-        main_ability:row[:main_ability], 
-        main_type:row[:main_type], 
-        base_experience: row[:base_exp]
-    )
-    trainer = Trainer.find_or_create_by(
-        name:row[:t_name], 
-        gender:row[:t_gender], 
-        home_region:[:t_region], 
-        team_member_status:row[:t_team_member], 
-        wins:row[:wins], 
-        losses:row[:losses]
-    )
-    
-    pokemon.trainers << trainer
+  pokemon = Pokemon.find_or_create_by(
+    name: row[:name],
+    main_ability: row[:main_ability],
+    main_type: row[:main_type],
+    base_experience: row[:base_exp]
+  )
+  trainer = Trainer.find_or_create_by(
+    name: row[:t_name],
+    gender: row[:t_gender],
+    home_region: row[:t_region],
+    team_member_status: row[:t_team_member],
+    wins: row[:wins],
+    losses: row[:losses]
+  )
 
-end 
+  pokemon.trainers << trainer
+end
 puts 'seeds.rb finishes here'
