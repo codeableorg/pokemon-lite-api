746faa6035e42b0f9a44913ab3c4c83babcf0a67
BenMaRe-Dev <b.maguinareyes@gmail.com>
2020-05-22 19:47:07 -0500

    Project done inclusive Optional

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,35 +1,69 @@
+# frozen_string_literal: true
+
 class PokemonsController < ApplicationController
+
+  before_action :load_trainer
+
   def index
-    @pokemon = Pokemon.all
-    render json: @pokemon
+    @pokemons = @trainer.present? ? @trainer.pokemons : Pokemon.all
+    @pokemons.all
+    render json: @pokemons
   end
+
   def show
-    @pokemon = Pokemon.find(params[:id])
+    @pokemons = @trainer.present? ? @trainer.pokemons : Pokemon.all
+    @pokemon = @pokemons.find(params[:id])
     render json: @pokemon
   end
+ 
   def create
-    @pokemon = Pokemon.new(pokemon_params)
+    if @trainer.present?
+      @pokemon = @trainer.pokemons.new(pokemon_params)
+      @pokemon.trainers << @trainer
+    else
+      @pokemon = Pokemon.new(pokemon_params)
+    end
+
     if @pokemon.save
       render json: @pokemon
     else
       render json: @pokemon.errors
     end
   end
+
   def update
-    @pokemon = Pokemon.find(params[:id])
+    if @trainer.present?
+       @pokemon = @trainer.pokemons.find(params[:id])  
+    else
+      @pokemon = Pokemon.find(params[:id])
+    end
+
     if @pokemon.update_attributes(pokemon_params)
       render json: @pokemon
     else
       render json: @pokemon.errors, status: :unprocessable_entity
     end
   end
+
   def destroy
-    @pokemon = Pokemon.find(params[:id])
-    @pokemon.destroy
-    render json: { status: 'Successfully destroyed', data: @pokemon }, status: :ok
+    if @trainer.present?
+      @pokemon = @trainer.pokemons.find(params[:id])
+      @trainer.pokemons.delete(@pokemon)
+      render json: { status: 'Successfully destroyed', data: @pokemon }, status: :ok
+    else
+      @pokemon = Pokemon.find(params[:id])
+      @pokemon.destroy
+      render json: { status: 'Successfully destroyed', data: @pokemon }, status: :ok
+    end
   end
+
+
   private
   def pokemon_params
     params.require(:pokemon).permit(:name, :experience, :main_type, :main_ability)
+  end
+
+  def load_trainer
+    @trainer = Trainer.find(params[:trainer_id]) if params.key?(:trainer_id)
   end
 end
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,47 +1,73 @@
 class TrainersController < ApplicationController
-  rescue_from ActiveRecord::RecordInvalid do |e|
-    render json: {error: e.message}, status: :unprocessable_entity
-  end
-
+  # rescue_from ActiveRecord::RecordInvalid do |e|
+  #   render json: {error: e.message}, status: :unprocessable_entity
+  # end
+  before_action :load_pokemon
   # GET /trainers
   def index
-    @trainers = Trainer.all
-    render json: @trainers, status: :ok
+    @trainers = @pokemon.present? ? @pokemon.trainers : Trainer.all
+    @trainers.all
+    render json: @trainers
   end
 
   # GET /trainers/{id}
   def show
-    @trainer = Trainer.find(params[:id])
-    render json: @trainer, status: :ok
+    @trainers = @pokemon.present? ? @pokemon.trainers : Trainer.all
+    @trainer = @trainers.find(params[:id])
+    render json: @trainer
   end
 
   # POST /trainers
   def create
-    @trainer = Trainer.create!(create_params)
-    render json: @trainer, status: :created
+    if @pokemon.present?
+      @trainer = @pokemon.trainers.new(trainer_params)
+      @trainer.pokemons << @pokemon
+    else
+      @trainer = Trainer.new(trainer_params)
+    end
+  
+    if @trainer.save
+      render json: @trainer
+    else
+      render json: @trainer.errors
+    end
   end
 
   # PUT /trainers/{id}
   def update
-    @trainer = Trainer.find(params[:id])
-    @trainer.update!(update_params)
-    render json: @trainer, status: :ok
+    if @pokemon.present?
+      @trainer = @pokemon.trainers.find(params[:id])
+    else
+      @trainer = Trainer.find(params[:id])
+    end
+
+    if @trainer.update!(trainer_params)
+      render json: @trainer
+    else
+      render json: @trainer.errors, status: :unprocessable_entity
+    end
   end
 
   #DELETE /trainers/{id}
   def destroy
-    @trainer = Trainer.find(params[:id])
-    @trainer.delete
-    render json: @trainer, status: :ok
+    if @pokemon.present?
+      @trainer = @pokemon.trainers.find(params[:id])
+      @pokemon.trainers.delete(@trainer)
+      render json: { status: 'Successfully destroyed', data: @trainer }, status: :ok
+    else
+      @trainer = Trainer.find(params[:id])
+      @trainer.destroy 
+      render json: { status: 'Successfully destroyed', data: @trainer }, status: :ok
+    end 
   end
 
   private
 
-  def create_params
-    params.permit(:name, :gender, :region, :team_member, :wins, :losses)
+  def trainer_params
+    params.require(:trainer).permit(:name, :gender, :region, :team_member, :wins, :losses)
   end
 
-  def update_params
-    params.permit(:name, :gender, :region, :team_member, :wins, :losses)
+  def load_pokemon
+  @pokemon = Pokemon.find(params[:pokemon_id]) if params.key?(:pokemon_id)
   end
 end
